90cd05f843f56c4153414a05a2deefa872ae1382
Ben W <merovex@hey.com>
2021-06-30 04:59:41 -0500

    Add Email flag to Posts.

---
+++ db/migrate/20210630095656_add_emailed_at_to_posts.rb
@@ -0,0 +1,5 @@
+class AddEmailedAtToPosts < ActiveRecord::Migration[6.1]
+  def change
+    add_column :posts, :emailed_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_152713) do
+ActiveRecord::Schema.define(version: 2021_06_30_095656) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_06_29_152713) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
+    t.datetime "emailed_at"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
